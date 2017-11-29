3e6061ce96b7cec8a06eaad444cf5415aa0c0d53
t0tem <vadimkul@gmail.com>
2017-11-29 18:09:40 +0100

    (C, sigma) for dataset 3 done

--- dataset3Params.m
+++ dataset3Params.m
@@ -24,10 +24,34 @@ sigma = 0.3;
 %
 
 
+options = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+results = zeros(0, 3);
 
+% code has been run just once and then commented (takes time to run).
+% result C & sigma hard coded below
 
+%for i = options
+%  for j = options
+%    C_temp = i;
+%    sigma_temp = j;
+%    model= svmTrain(X, y, C_temp, @(x1, x2) gaussianKernel(x1, x2, sigma_temp));
+%    predictions = svmPredict(model, Xval);
+%    error = mean(double(predictions ~= yval));
+%    results = [results;[C_temp, sigma_temp, error]];
+%  end
+%end
 
+%[dummy, row] = min(results,[],1); %now 3rd value of vector row (i.e. row(3) 
+% holds the row number of results matrix with minumum error and corresponding
+% C and sigma values
+%
+%C = results(row(3),1);
+%sigma = results(row(3),2);
+
+% hardcoding found optimal parameters
+C = 1;
+sigma = 0.1;
 
 % =========================================================================
 
